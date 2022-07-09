b04ed9f93c86aa97227b22f7ab6a3a47ffbc367d
Deep Mondal <deep.mondal.2020@gmail.com>
2022-07-09 10:26:42 +0530

    second commit

--- Employeeform/settings.py
+++ Employeeform/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-biq*#ib74-d#rig*-&ep2g(4ld)w31kwtrn-yb*ugb4op-5vmo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','hireonboard.herokuapp.com']
 
 
 # Application definition
